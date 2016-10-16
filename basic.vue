84377d34e66f3582cfe8909e5262ee697d940ec3
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-10-16 12:03:15 +0200

    Updates

--- basic.vue
+++ basic.vue
@@ -1,26 +1,62 @@
-<template lang="jade">
+<template lang="pug">
 .container
-  a(href="https://github.com/paulpflug/vue-clusterize/blob/master/dev/basic.vue") source
-  clusterize(v-ref:clusterize v-bind:data="rowsData" v-bind:height="400" style="width:200px")
-    clusterize-row
-      {{data}}
-    p(slot="loading") loading
+  button(@click="openOverlay") Open Overlay
+  button(
+    @click.prevent="openSecondOverlay",
+    :style="buttonStyle",
+    v-if="firstOpened"
+    ) Open blue Overlay
+  button(
+    @click.prevent="closeFirstOverlay",
+    :style="button2Style",
+    v-if="bothOpened"
+    ) Close bottom Overlay first
+  br
+  button(@click.prevent="openScrollOverlay") Overlay with allowScroll
+  a(href="https://github.com/vue-comps/vue-overlay/blob/master/dev/basic.vue",style="margin: 20px") source
+  div(style="margin:auto;text-align:center;height:2000px") something centered (shouldn't move on disapearing of the scrollbar)
 </template>
 
 <script lang="coffee">
 module.exports =
-  components:
-    "clusterize": require "../clusterize.js"
-    "clusterize-row": require "../clusterize-row.js"
+  mixins:[
+    require("vue-mixins/vue")
+  ]
+  computed:
+    bothOpened: -> @firstOpened and @secondOpened
   data: ->
-    rowsData: (x for x in [1..10000])
-</script>
+    firstOpened: false
+    secondOpened: false
+    buttonStyle:
+      position:"relative"
+      zIndex: 0
+    button2Style:
+      position:"relative"
+      zIndex: 0
+  methods:
+    openOverlay: (cb) ->
+      @result = @overlay.open
+        onBeforeOpen: =>
+          @firstOpened = true
+        onBeforeClose: =>
+          @firstOpened = false
+        onOpened: cb
+      @buttonStyle.zIndex = @result.zIndex
+    openSecondOverlay: (cb) ->
+      result = @overlay.open
+        opacity:0.6
+        color: "blue"
+        onBeforeOpen: =>
+          @secondOpened = true
+        onBeforeClose: =>
+          @secondOpened = false
+        onOpened:cb
+      @button2Style.zIndex = result.zIndex
+    openScrollOverlay: ->
+      @overlay.open allowScroll:true
+    closeFirstOverlay: ->
+      @result.close()
+  mounted: ->
+    @overlay = require("../overlay.js")(@Vue)
 
-<style lang="stylus">
-.clusterize
-  border 1px solid black
-.container > a
-  position absolute
-  left 250px
-  top 40px
-</style>
+</script>
